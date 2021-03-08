0171706e4241ec464c924bd7228d39abc708be16
Sara Patel <gallegosara@hotmail.com>
2021-03-08 17:53:52 -0500

    adding flask

--- app.py
+++ app.py
@@ -73,27 +73,51 @@ def stations():
 
     session = Session(engine)
 
-    results = session.query(stations.name).all()
+    results = session.query(Station.name).all()
 
     session.close()
 
-    # Convert list of tuples into normal list
-    all_stations = list(np.ravel(results))
-
-    return jsonify(all_stations)
+    return jsonify(results)
 
 #/api/v1.0/tobs
 #Query the dates and temperature observations of the most active station for the last year of data.
 #Return a JSON list of temperature observations (TOBS) for the previous year.
+@app.route("/api/v1.0/tobs")
+def tobs():
+
+    session = Session(engine)
+
+    stmt = session.query(Measurement.station, func.count('*').label("station_count")).group_by(Measurement.station).subquery()
+
+    topstation = ""
+    topcount = 0
+    for station, count in session.query(Measurement, stmt.c.station_count).outerjoin(stmt, Measurement.station == stmt.c.station).group_by(Measurement.station):
+        if count > topcount:
+            topstation = station
+            topcount = count 
+    print(topstation,topcount)
+    results = session.query(Measurement.date,Measurement.station,Measurement.tobs).filter(Measurement.station == topstation.station).all()
 
 
-
+    return jsonify(results) 
 #/api/v1.0/<start> and /api/v1.0/<start>/<end>
 #Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
 #When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
 #When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive
 
+@app.route("/api/v1.0/<start>", defaults={'end': ""})
+@app.route("/api/v1.0/<start>/<end>")
+def start(start,end ):
 
+    if end == "":
+        end = dt.date.today()
+        print("EndDate",end)
+    session = Session(engine)
+
+    results = session.query(Measurement.date,Measurement.station,Measurement.tobs).filter(Measurement.date.between(start , end)).all()
+
+
+    return jsonify(results)
 
 
 
